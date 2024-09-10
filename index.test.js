098b5b604cfbcad8d7c30d872b51ec8d3292c38e
karlijones <karlijones1984@gmail.com>
2024-09-10 13:00:35 -0500

    writing more tests

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { forEach } = require('./index');
+const { forEach, map } = require('./index');
 
 let sum = 0;
 forEach([1,2,3], (value) => {
@@ -8,3 +8,9 @@ forEach([1,2,3], (value) => {
 if (sum !== 6) {
     throw new Error('Expected summing array to equal 6');
 }
+
+const result = map([1,2,3], value => {
+    return value * 2;
+});
+
+// result = [2, 4, 6]
