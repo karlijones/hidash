95c1ff9819ec6e1510fee9c2919923182c7b2844
karlijones <karlijones1984@gmail.com>
2024-09-10 13:04:41 -0500

    test for map

--- index.test.js
+++ index.test.js
@@ -13,4 +13,12 @@ const result = map([1,2,3], value => {
     return value * 2;
 });
 
-// result = [2, 4, 6]
+if (result[0] !== 2) {
+    throw new Error(`Expected to find 2, but found ${result[0]}`)
+}
+if (result[1] !== 4) {
+    throw new Error(`Expected to find 4, but found ${result[0]}`)
+}
+if (result[2] !== 6) {
+    throw new Error(`Expected to find 6, but found ${result[0]}`)
+}
